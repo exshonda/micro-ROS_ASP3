1c66abc639629e8f906f24d3791a58e70c6de3f7
h7ga40 <hi6aki_7ga40@hotmail.com>
2022-10-06 20:36:43 +0900

    ターゲットごとのフォルダに分割

--- micro_ros_arduino/micro_ros_arduino.mk
+++ micro_ros_arduino/micro_ros_arduino.mk
@@ -1,9 +1,9 @@
 
-APPL_DIRS += ../micro_ros_arduino/src
+APPL_DIRS += ../micro_ros_arduino/$(TARGET)
 APPL_CXXOBJS += micro_ros_arduino.o
 APPL_CXXOBJS += pinmode.o
 
-INCLUDES += -I../micro_ros_arduino/src
+INCLUDES += -I../micro_ros_arduino/$(TARGET)
 INCLUDES += -I../microros/install/include
 
 LDFLAGS += -L../microros
---
+++ micro_ros_arduino/nucleo_f767zi_gcc/micro_ros_arduino.h
@@ -0,0 +1,28 @@
+
+#ifndef MICRO_ROS_ARDUINO
+#define MICRO_ROS_ARDUINO
+
+// ---- Build fixes -----
+//Removing __attribute__ not supported by gcc-arm-none-eabi-5_4
+#define __attribute__(x)
+
+#include <uxr/client/transport.h>
+#include <rmw_microros/rmw_microros.h>
+
+extern "C" bool arduino_transport_open(struct uxrCustomTransport * transport);
+extern "C" bool arduino_transport_close(struct uxrCustomTransport * transport);
+extern "C" size_t arduino_transport_write(struct uxrCustomTransport* transport, const uint8_t * buf, size_t len, uint8_t * err);
+extern "C" size_t arduino_transport_read(struct uxrCustomTransport* transport, uint8_t* buf, size_t len, int timeout, uint8_t* err);
+
+static inline void set_microros_transports(int port){
+	rmw_uros_set_custom_transport(
+		true,
+		(void *)port,
+		arduino_transport_open,
+		arduino_transport_close,
+		arduino_transport_write,
+		arduino_transport_read
+	);
+}
+
+#endif  // MICRO_ROS_ARDUINO
