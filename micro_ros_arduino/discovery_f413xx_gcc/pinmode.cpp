ecabf412a8fd6ceeb4ba59589f9692f2dbed7441
Hiroaki Nagashima <hi6aki_7ga40@hotmail.com>
2022-10-06 21:21:54 +0900

    ビルドが通るよう更新

---
+++ micro_ros_arduino/discovery_f413xx_gcc/pinmode.cpp
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include "pinmode.h"
+
+int  digitalRead(uint8_t no)
+{
+	return 0;
+}
+
+void digitalWrite(uint8_t no, int sw)
+{
+}
+
+void pinMode(uint8_t no, uint32_t mode)
+{
+}
+
---
+++ micro_ros_arduino/discovery_f413xx_gcc/pinmode.h
@@ -0,0 +1,28 @@
+#ifndef _PINMODE_H_
+#define _PINMODE_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#define DIGITAL_PIN     0
+#define ANALOG_PIN      1
+
+#ifndef TOPPERS_MACRO_ONLY
+
+/*
+ *  関数のプロトタイプ宣言
+ */
+
+extern int  digitalRead(uint8_t no);
+extern void digitalWrite(uint8_t no, int sw);
+extern void pinMode(uint8_t no, uint32_t mode);
+
+#endif /* TOPPERS_MACRO_ONLY */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* _PINMODE_H_ */
+
