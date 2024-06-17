dfe4cc40bade9aace0b047611e1c0ed6da1a5dc2
exhonda <honda@ertl.jp>
2024-06-17 23:05:29 +0900

    deploy-win/deploy-lin を追加

--- spike-rt/spike-rt.mk
+++ spike-rt/spike-rt.mk
@@ -1,3 +1,14 @@
 SPIKE-RT_DIR = ../../../spike-rt
 
 SPIKE-RT_KERNELLIB_DIR = $(SPIKE-RT_DIR)/build/obj-primehub_kernel/
+
+DFU_UTIL_WIN = $(SPIKE-RT_DIR)/dfu-util-0.11-binaries/win64/dfu-util.exe
+DFU_UTIL_LIN = $(SPIKE-RT_DIR)/scripts/deploy-dfu.sh
+
+deploy-win: asp.bin
+	@echo "Writing $< to the board"
+	$(DFU_UTIL_WIN) -a 0 -d 0x0694:0x0008 -s 0x8008000 -R -D $(OBJNAME).bin
+
+deploy-lin: asp.bin
+	@echo "Writing $< to the board"
+	sudo PYTHON3=python3 $(DFU_UTIL_LIN) $(OBJNAME).bin
